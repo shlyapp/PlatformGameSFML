d74264de58c9ccc9cdeec61df4c97c7d223f0fc2
shlyapp <78727203+shlyapp@users.noreply.github.com>
2021-08-14 13:32:33 +0400

    Подключение библиотеки SFML.

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,30 @@
 ﻿
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "YouGame");
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
 }
+
 
