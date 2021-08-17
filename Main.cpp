a427c0f195cabda0d6baf8fafb1e2f2f06053372
shlyapp <78727203+shlyapp@users.noreply.github.com>
2021-08-18 00:30:00 +0400

    Комментарии к Main.

--- Main.cpp
+++ Main.cpp
@@ -1,34 +1,39 @@
-﻿#include <iostream>
+﻿//////////////////////////////////////////////////////////////
+/// Необходимые библиотеки.
+//////////////////////////////////////////////////////////////
+
+#include <iostream>
 #include <list>
 #include <string>
 #include <fstream>
 
 #include <SFML/Graphics.hpp>
 
+// Определяем для удобного взаимодействия, типо "свой тип данных"
 using Rect2f = sf::Vector2<sf::Vector2f>;
 
 //////////////////////////////////////////////////////////////
 /// Подключение заголовочных файлов с кодом.
 //////////////////////////////////////////////////////////////
 
-#include "Entity.h"
-#include "GUI.h"
-#include "GameEvent.h"
-#include "Level.h"
-#include "Collision.h"
-#include "Abilities.h"
-#include "GameObject.h"
-#include "InterfaceUI.h"
-
-#include "Game.h"
-#include "Scene.h"
+#include "Entity.h" // <-- Базовый класс для почти всего.
+#include "GUI.h" // <-- Графический интерфейс, создание кнопок.
+#include "GameEvent.h" // <-- События игры.
+#include "Level.h" // <-- Уровни, карта.
+#include "Collision.h" // <-- Столкновения.
+#include "Abilities.h" // <-- Интерфейсы - возможности.
+#include "GameObject.h" // <-- Все игровые обькты.
+#include "InterfaceUI.h" // <-- Меню, блоки информации.
+#include "Game.h" // <-- Сама игра, обновления.
+#include "Scene.h" // <-- Сцена.
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "YouGame");
     window.setFramerateLimit(60);
 
-    Scene scene(&window);
+    // Создаем сцену.
+    Scene scene(&window); 
 
     while (window.isOpen())
     {
@@ -44,6 +49,7 @@ int main()
         }
 
         window.clear();
+        // Выводим сцену, а в ней уже выводится все остальное в зависимости от состояния игры.
         window.draw(scene);
         window.display();
     }
