ab24647ce58f618c2d81ecd7c2bb8b609e8f64c8
shlyapp <78727203+shlyapp@users.noreply.github.com>
2021-08-18 00:25:05 +0400

    Комментарии к Game.

--- Game.h
+++ Game.h
@@ -2,6 +2,10 @@
 
 class Game;
 
+//////////////////////////////////////////////////////////////
+/// Этот класс отвечает за первичную загрузку компонентов игры:
+/// обьектов, уровней и так же загрузку при рестарте.
+//////////////////////////////////////////////////////////////
 class GameLoader
 {
 private:
@@ -17,6 +21,9 @@ public:
 
 };
 
+//////////////////////////////////////////////////////////////
+/// Этот класс отвечает за обновление всех компонентов игры.
+//////////////////////////////////////////////////////////////
 class GameUpdater : public IGameEventMaker
 {
 private:
@@ -32,6 +39,9 @@ public:
 
 };
 
+//////////////////////////////////////////////////////////////
+/// Класс игры, хранит в себе все компоненты.
+//////////////////////////////////////////////////////////////
 class Game : 
 	public sf::Drawable, 
 	public IGameEventListener, 
@@ -43,24 +53,30 @@ class Game :
 
 private:
 
+	// Количество жизней персонажа.
 	int lives_;
 
+	// Время.
 	sf::Clock* game_clock_;
 	sf::Clock* clock_;
 	mutable float game_time_;
 
+	// Вид и окно.
 	sf::View* view_;
 	sf::RenderWindow* window_;
 
+	// Главный игрок, который всегда на экране и два, между которым переключается.
 	Player* main_player_;
 	Player* player1_;
 	Player* player2_;
 
+	// Информация о игроке и боссе.
 	PlayerInfoBar* info_;
 	BossInfoBar* boss_info_;
 
 public:
 
+	// Стандарная инициализация.
 	Game(sf::RenderWindow* window, sf::View* view) :
 		view_(view),
 		window_(window),
@@ -74,6 +90,7 @@ public:
 
 	void draw(sf::RenderTarget& target, sf::RenderStates state) const override
 	{
+		// Оповещаем всех о том, что мы в игре.
 		notifyListeners(GameEventState::inGame);
 
 		// Работа со временем.
@@ -115,28 +132,34 @@ public:
 		
 	}
 
+	// Обновляем игру в зависимости от события.
 	void updateByGameEvent(GameEventState event) override
 	{
 		switch (event)
 		{
+			// Если началась игра, то загружаем ее и оповещаем слушателей.
 		case GameEventState::StartGame:
 			GameLoader::loadGame(this, view_);
 			notifyListeners(GameEventState::inGame);
 			break;
 
+			// Если умирает игрок, то уменьшаем количество жизней и пересоздаем игрока.
 		case GameEventState::PlayerDied:
 			lives_--;
 			restartPlayer();
 			break;
 
+			// Если меняется игрок, то вызываем соответсвующий метод.
 		case GameEventState::PlayerChanged:
 			changePlayer();
 			break;
 
+			// Если новый уровень, то пересоздаем персонажа.
 		case GameEventState::SetNewLevel:
 			restartPlayer();
 			break;
 
+			// Если игра начинается снова, то удаляем предыдущие уровни и загружаем все снова.
 		case GameEventState::RestartGame:
 			LevelManager::deleteLevels();
 			GameLoader::loadGame(this, view_);
@@ -145,10 +168,13 @@ public:
 
 	}
 
+	// Метод смены игрока. main_player_ просто переключается между двумя игроками.
 	void changePlayer()
 	{
+		// Если уже первый игрок
 		if (main_player_ == player1_)
 		{
+			// То ставим второго
 			main_player_ = player2_;
 		}
 		else if (main_player_ == player2_)
@@ -157,51 +183,66 @@ public:
 		}
 	}
 
+	// Перезагрузка игрока
 	void restartPlayer()
 	{
+		// Жизни закончились, то заканчиваем игру.
 		if (lives_ == 0)
 		{
 			notifyListeners(GameEventState::GameOver);
 		}
 
+		// Иначе создаем новых игроков.
 		player1_ = new Player(LevelManager::level->start_position, { 49, 112 }, view_, "data/images/player.png");
 		player2_ = new Player(LevelManager::level->start_position, { 49, 112 }, view_, "data/images/player.png");
 		main_player_ = player1_;
 	}
 
+	// При перезапуске игры.
 	void restartGame()
 	{
+		// Игровое время обнуляем.
 		game_clock_->restart();
 
+		// Снова 3 жизни.
 		lives_ = 3;
+		// Ставим начальный уровень.
 		LevelManager::setLevel(0);
 	}
 
 };
 
+// Тут происходят все главные обновления
 inline void GameUpdater::gameUpdate(const Game* game, float time)
 {
 	// Обновляем и проверяем на столкновение.
 
+	// Обновляем текущего игрока.
 	game->main_player_->update(time);
 
+	// Для каждого блока в блоках карты.
 	for (auto block : LevelManager::level->map->blocks)
 	{
+		// Проверяем на столковени.
 		if (game->main_player_->getRect2f() & block->getRect2f())
 		{
+			// Если столкнулись, то вызываем соответственный метод.
 			game->main_player_->handlingCollision(*block);
 		}
 	}
 
+	// Для каждого предмета.
 	for (auto it = LevelManager::level->map->items.begin(); it != LevelManager::level->map->items.end(); ++it)
 	{
 		Item* item = *it;
 
+		// Если их нет, то завершаем цикл.
 		if (LevelManager::level->map->items.size() == 0)
 		{
 			break;
 		}
-
+		
+		// Проверяем на столкновние с персонажем.
 		if (game->main_player_->getRect2f() & item->getRect2f())
 		{
 			game->main_player_->handlingCollision(*item);
@@ -213,6 +254,7 @@ inline void GameUpdater::gameUpdate(const Game* game, float time)
 			break;
 		}
 
+		// Если предет не активен, то удаляем его.
 		if (!item->isActive())
 		{
 			it = LevelManager::level->map->items.erase(it);
@@ -220,6 +262,7 @@ inline void GameUpdater::gameUpdate(const Game* game, float time)
 		}
 	}
 
+	// Аналогично предыдущему.
 	for (auto it = LevelManager::level->enemys.begin(); it != LevelManager::level->enemys.end(); ++it)
 	{
 		Enemy* enemy = *it;
@@ -239,6 +282,7 @@ inline void GameUpdater::gameUpdate(const Game* game, float time)
 		}
 	}
 
+	// Аналогично: обновляем и проверяем на столкновение.
 	for (auto moving_platform : LevelManager::level->moving_platforms)
 	{
 		moving_platform->update();
@@ -249,6 +293,7 @@ inline void GameUpdater::gameUpdate(const Game* game, float time)
 		}
 	}
 	
+	// Так же, но еще обновляем пули и информацию.
 	if (LevelManager::level->boss != nullptr)
 	{
 		if (LevelManager::level->boss->isLive())
@@ -265,22 +310,28 @@ inline void GameUpdater::gameUpdate(const Game* game, float time)
 		}
 	}
 
+	// Обновляем блок информации персонажа.
 	game->info_->update(game->main_player_->getHealth(), game->lives_, game->game_time_, game->main_player_->getGearsNum(), *game->view_);
-
-	if (!LevelManager::level->boss->isLive())
-	{
-		notifyListeners(GameEventState::WinGame);
-	}
 }
 
 inline void GameLoader::loadGame(Game* game, sf::View* view)
 {
+	// Перезапускаем часы отсчета времени.
 	game->game_clock_->restart();
 
+	// Устаналиваем три жизни игрока.
 	game->lives_ = 3;
 	 
 	// Загрузка уровней.
 
+	//////////////////////////////////////////////////////////////
+	/// Как происходит загрузка уровней?
+	/// 1) Создаем сам уровень, для этого нам надо:
+	///		1.1) Создать карту, указав на ее размеры и местополжение в памяти
+	///		1.2) Указать стартовую позицию на уровне.
+	/// 2) Добавляем необходимые игровые обьекты, такие как:
+	///		передвигающая платформа, враг, босс.
+	//////////////////////////////////////////////////////////////
 	Level* level1(new Level(new Map(sf::Vector2f{ 26, 15 }, "data/images/textures.png", "data/maps/firstMap.txt"), sf::Vector2f{ 100, 100 }));
 	level1->addMovingPlatform(new MovingPlatform(200, sf::Vector2f{ 0, 500 }, sf::Vector2f{ 150, 50 }, "data/images/platform.png"));
 	level1->addEnemy(new Enemy(340, sf::Vector2f{ 200, 500 }, sf::Vector2f{ 35, 48 }, "data/images/chip.png"));
